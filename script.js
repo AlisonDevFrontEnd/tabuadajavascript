3054d19efc114fbf84307941f7a8a67c7a49ad1f
AlisonDevFrontEnd <150954455+AlisonDevFrontEnd@users.noreply.github.com>
2024-09-29 10:56:30 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function multiplicationTable() {
-  const resultDiv = document.getElementById("result");
-  for (let i = 2; i <= 9; i++) {
-    const container = document.createElement("div");
-    container.classList.add("tabuada");
-    const title = document.createElement("h3");
-    title.innerText = `Tabuada do ${i}:`;
-    container.appendChild(title);
-    for (let j = 1; j <= 10; j++) {
-      const paragraph = document.createElement("p");
-      paragraph.innerText = `${i} x ${j} = ${i * j}`;
-      container.appendChild(paragraph);
-    }
-    resultDiv.appendChild(container);
+const resultDiv = document.querySelector('.result');
+for (let i = 2; i <= 9; i++) {
+  const container = document.createElement('div');
+  container.classList.add('multiplication-table');
+  const title = document.createElement('h3');
+  title.innerText = `Tabuada do ${i}:`;
+  container.appendChild(title);
+  for (let j = 1; j <= 10; j++) {
+    const p = document.createElement('p');
+    p.innerText = `${i} x ${j} = ${i * j}`;
+    container.appendChild(p);
   }
+  resultDiv.appendChild(container);
 }
-multiplicationTable();
