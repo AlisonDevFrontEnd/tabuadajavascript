e1bf5b5e36ef1b9be9e08691257eeb4130578c50
AlisonDevFrontEnd <150954455+AlisonDevFrontEnd@users.noreply.github.com>
2024-10-05 18:47:35 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const resultDiv = document.querySelector('.result');
+const resultMain = document.querySelector(".container");
+
 for (let i = 2; i <= 9; i++) {
-  const container = document.createElement('div');
-  container.classList.add('multiplication-table');
-  const title = document.createElement('h3');
+  const containerSection = document.createElement("section");
+  containerSection.classList.add("multiplication-table");
+  const title = document.createElement("h3");
   title.innerText = `Tabuada do ${i}:`;
-  container.appendChild(title);
+  containerSection.appendChild(title);
+  
   for (let j = 1; j <= 10; j++) {
-    const p = document.createElement('p');
-    p.innerText = `${i} x ${j} = ${i * j}`;
-    container.appendChild(p);
+    const paragraph = document.createElement("p");
+    paragraph.innerText = `${i} x ${j} = ${i * j}`;
+    containerSection.appendChild(paragraph);
   }
-  resultDiv.appendChild(container);
+  resultMain.appendChild(containerSection);
 }
